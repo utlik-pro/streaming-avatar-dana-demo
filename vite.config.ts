515230ac15f7e3c962f386c74392d57b8fdc09c0
Xu Zhipei <xuzhipei@gmail.com>
2025-01-07 15:42:57 +0800

    fix: base url in github page env

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/streaming/avatar`,
+  base: process.env.GITHUB_ACTIONS ? '/akool-streaming-avatar-react-demo' : '/streaming/avatar',
   server: {
     host: '0.0.0.0'
   },
